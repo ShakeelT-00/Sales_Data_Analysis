7c7b3e78f4fb3ace2d272283ec3bb21c8e267410
ShakeelT-00 <tanveershakeel14@gmail.com>
2025-03-26 14:36:14 -0400

    Changes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
 import pandas as pd
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
